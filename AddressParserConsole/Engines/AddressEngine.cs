128fe4c45f445ca59878f7659b4e4b2eaa14d079
Ben Reis <benjaminreis@gmail.com>
2019-06-17 23:12:24 -0500

    moving into engine

---
+++ AddressParserConsole/Engines/AddressEngine.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace AddressParserConsole.Engines
+{
+    public class AddressEngine
+    {
+        public AddressEngine()
+        {
+        }
+
+
+        internal string HandleWashingtonSt(int nHouseNumber)
+        {
+            if (nHouseNumber <= 1499 && nHouseNumber >= 1300)
+            {
+                return "17";
+            }
+            if (nHouseNumber <= 1699 && nHouseNumber >= 1500)
+            {
+                return "9";
+            }
+            if (nHouseNumber <= 1999 && nHouseNumber >= 1700)
+            {
+                return "11";
+            }
+            if (nHouseNumber <= 2199 && nHouseNumber >= 2000)
+            {
+                return "10";
+            }
+            if (nHouseNumber <= 2399 && nHouseNumber >= 2200)
+            {
+                return "18";
+            }
+            if (nHouseNumber <= 2499 && nHouseNumber >= 2400)
+            {
+                if (nHouseNumber % 2 == 0)
+                {
+                    return "12";
+                }
+                else
+                {
+                    return "18";
+                }
+            }
+            if (nHouseNumber <= 2699 && nHouseNumber >= 2500)
+            {
+                return "12";
+            }
+            return "washington section unknown";
+        }
+
+
+        internal string HandleHarwood(int nHouseNumber)
+        {
+
+            if (nHouseNumber <= 1699 && nHouseNumber > -1600)
+            {
+                return "13";
+            }
+            if (nHouseNumber <= 1999 && nHouseNumber >= 1700)
+            {
+                return "14";
+            }
+            if (nHouseNumber < 2399 && nHouseNumber >= 2000)
+            {
+                return "15";
+            }
+
+            return "harwood section unknown";
+        }
+    }
+}
--- AddressParserConsole/Managers/AddressManager.cs
+++ AddressParserConsole/Managers/AddressManager.cs
@@ -10,7 +10,7 @@ namespace AddressParserConsole.Managers
         }
 
         private Accessors.AddressAccessor _AddressAccessor = new Accessors.AddressAccessor();
-
+        private Engines.AddressEngine _AddressEngine = new Engines.AddressEngine();
 
         internal List<Models.AddressModel> ProcessAddresses()
         {
@@ -24,57 +24,23 @@ namespace AddressParserConsole.Managers
 
                 if (address.StreetName.Equals("washington", StringComparison.OrdinalIgnoreCase))
                 {
-                    if (address.nHouseNumber % 2 == 0 )
-                    {
-                        //
-                    }
-
-                    if (address.nHouseNumber <= 1499 && address.nHouseNumber >= 1300)
-                    {
-                        address.Section = "17";
-                    }
-                    if (address.nHouseNumber <= 1699 && address.nHouseNumber >= 1500)
-                    {
-                        address.Section = "9";
-                    }
-                    if (address.nHouseNumber <= 1999 && address.nHouseNumber >= 1700)
-                    {
-                        address.Section = "11";
-                    }
-                    if (address.nHouseNumber <= 2199 && address.nHouseNumber >= 2000)
-                    {
-                        address.Section = "10";
-                    }
-                    if ( address.nHouseNumber <= 2399 && address.nHouseNumber >= 2200)
-                    {
-                        address.Section = "18";
-                    }
-                    if (address.nHouseNumber <= 2499 && address.nHouseNumber >= 2400)
-                    {
-                        if (address.nHouseNumber % 2 == 0)
-                        {
-                            address.Section = "12";
-                        }
-                        else
-                        {
-                            address.Section = "18";
-                        }
-                    }
-                    if (address.nHouseNumber <= 2699 && address.nHouseNumber >= 2500)
-                    {
-                        address.Section = "12";
-                    }
-
+                   address.Section = _AddressEngine.HandleWashingtonSt(address.nHouseNumber);
                 }
-                
+
                 #endregion
+
+
+                #region "harwood"
+
                 if (address.StreetName.Equals("harwood", StringComparison.OrdinalIgnoreCase))
                 {
-                    if (address.nHouseNumber <= 1999 && address.nHouseNumber >= 1700)
-                    {
-                        address.Section = "15";
-                    }
+                    address.Section = _AddressEngine.HandleHarwood(address.nHouseNumber);
                 }
+
+
+
+
+                #endregion
 
             }
 
