949fbdbe05c754be0299483999d4eb9cd15429a1
Ben Reis <benjaminreis@gmail.com>
2019-06-17 22:51:16 -0500

    washington st.

--- AddressParserConsole/Managers/AddressManager.cs
+++ AddressParserConsole/Managers/AddressManager.cs
@@ -20,6 +20,8 @@ namespace AddressParserConsole.Managers
 
             foreach (Models.AddressModel address in Addresses)
             {
+                #region "Washington"
+
                 if (address.StreetName.Equals("washington", StringComparison.OrdinalIgnoreCase))
                 {
                     if (address.nHouseNumber % 2 == 0 )
@@ -35,9 +37,37 @@ namespace AddressParserConsole.Managers
                     {
                         address.Section = "9";
                     }
-
+                    if (address.nHouseNumber <= 1999 && address.nHouseNumber >= 1700)
+                    {
+                        address.Section = "11";
+                    }
+                    if (address.nHouseNumber <= 2199 && address.nHouseNumber >= 2000)
+                    {
+                        address.Section = "10";
+                    }
+                    if ( address.nHouseNumber <= 2399 && address.nHouseNumber >= 2200)
+                    {
+                        address.Section = "18";
+                    }
+                    if (address.nHouseNumber <= 2499 && address.nHouseNumber >= 2400)
+                    {
+                        if (address.nHouseNumber % 2 == 0)
+                        {
+                            address.Section = "12";
+                        }
+                        else
+                        {
+                            address.Section = "18";
+                        }
+                    }
+                    if (address.nHouseNumber <= 2699 && address.nHouseNumber >= 2500)
+                    {
+                        address.Section = "12";
+                    }
 
                 }
+                
+                #endregion
                 if (address.StreetName.Equals("harwood", StringComparison.OrdinalIgnoreCase))
                 {
                     if (address.nHouseNumber <= 1999 && address.nHouseNumber >= 1700)
